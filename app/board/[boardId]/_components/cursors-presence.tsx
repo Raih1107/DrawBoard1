903310b7ef9b2366570c75b2d82ee68575db36ac
Raih1107 <raih68737@gmail.com>
2025-07-26 15:35:29 +0530

    Refactor CursorsPresence component to streamline rendering of drafts and remove unused Cursors component

--- app/board/[boardId]/_components/cursors-presence.tsx
+++ app/board/[boardId]/_components/cursors-presence.tsx
@@ -7,57 +7,39 @@ import { Path } from "./path";
 import { colorToCss } from "@/lib/utils";
 
 
-const Cursors = () => {
-    const ids = useOthersConnectionIds();
-
-
-    return (
-        <>
-            {ids.map((connectionId) => (
-                <Cursor 
-                    key={connectionId}
-                    connectionId={connectionId}
-                />
-            ))}
-        </>
-    )
-}
+export const CursorsPresence = () => {
+  return (
+    <>
+      <Drafts />
+    </>
+  );
+};
 
 const Drafts = () => {
-    const others = useOthersMapped((other) => ({
-        pencilDraft: other.presence.pencilDraft,
-        penColor: other.presence.penColor,
-    }) , shallow);
+  const others = useOthersMapped(
+    (other) => ({
+      pencilDraft: other.presence?.pencilDraft,
+      penColor: other.presence?.penColor,
+    }),
+    shallow
+  );
 
-
-    return(
-        <>
-        {others.map(([key, other]) => {
-            if(other.pencilDraft){
-                return(
-                    <Path 
-                        key={key}
-                        x={0}
-                        y={0}
-                        points={other.pencilDraft}
-                        fill={other.penColor ? colorToCss(other.penColor) : "#FFF"}
-                    />
-                )
-            }
-        })}
-        </>
-    )
-}
-
-export const CursorsPresence = memo(() => {
-
-    return(
-        <>
-            <Drafts />
-            <Cursors />
-        </>
-    )
-});
-
-
-CursorsPresence.displayName = "CursorsPresence";
+  return (
+    <>
+      {others.map(([key, other]) => {
+        if (other.pencilDraft) {
+          return (
+            <Path
+              key={key}
+              x={0}
+              y={0}
+              points={other.pencilDraft}
+              fill={other.penColor ? colorToCss(other.penColor) : "#FFF"}
+            />
+          );
+        }
+        return null;
+      })}
+    </>
+  );
+};
