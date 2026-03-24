33f3ed034186566a605eb8d1ab27468113a56375
Raih1107 <raih68737@gmail.com>
2026-03-24 18:05:23 +0530

    feat: implement a real-time collaborative canvas component and clean up the README.

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -36,7 +36,7 @@ export const Canvas = ({
 
     const [camera, setCamera] = useState<Camera>({ x : 0, y : 0});
     const [lastUsedColor, setLastUsedColor] = useState<Color>({
-        r:0, g:0, b:0,
+        r:255, g:255, b:255,
     });
 
     useDisableScrollBounce();
